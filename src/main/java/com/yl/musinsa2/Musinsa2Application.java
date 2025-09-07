34084fa183c46f56de3617d32b71a01e1e41d4f5
moon maria <tatujjang@gmail.com>
2025-09-07 15:01:52 +0900

    first

---
+++ src/main/java/com/yl/musinsa2/Musinsa2Application.java
@@ -0,0 +1,13 @@
+package com.yl.musinsa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Musinsa2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Musinsa2Application.class, args);
+    }
+
+}
